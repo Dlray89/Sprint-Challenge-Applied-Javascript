40f03a93ced530523b675bb7a4b99844523d54d2
DAP <58054581+Dlray89@users.noreply.github.com>
2020-01-17 14:11:03 -0500

    update

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(dates, title, temp) {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const Title = document.createElement('h1');
+    const temperature = document.createElement('span');
+
+    header.classList.add("header");
+    date.classList.add("date");
+    temperature.classList.add('temp');
+
+    header.appendChild(date);
+    header.appendChild(Title);
+    header.appendChild(temperature);
+
+    date.textContent = "March 28, 2019";
+    Title.textContent = "Lambda Times";
+    temperature.textContent = "98°"
+
+    return header
+}
+const attachdoc = document.querySelector('.header-container')
+attachdoc.appendChild(Header());
+console.log(attachdoc)
